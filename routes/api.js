f0b74b39524427eaca4f8f228fe4707a8cbdcc02
charmiainemimie <charmainemangorima@gmail.com>
2024-05-03 12:10:39 +0200

    error handling when posting to the db

--- routes/api.js
+++ routes/api.js
@@ -3,25 +3,25 @@ const router=express.Router()
 const Ninja=require('../models/ninja')
 
 //get a list of ninjas from the db-GET
-router.get('/ninjas',(req,res)=>{
+router.get('/ninjas',(req,res,next)=>{
     res.send({type:'GET'})
 } )
 
 //add a new ninja to the db-POST
-router.post('/ninjas',(req,res)=>{
+router.post('/ninjas',(req,res,next)=>{
     Ninja.create(req.body)  //returns a promise so use .then
         .then((ninja)=>{
              res.send(ninja)
-        }) 
+        }).catch(next) 
 } )
 
 //update a ninja in the db-PUT
-router.put('/ninjas/:id',(req,res)=>{
+router.put('/ninjas/:id',(req,res,next)=>{
     res.send({type:'PUT'})
 } )
 
 //delete a ninja from the db-DELETE
-router.delete('/ninjas/:id',(req,res)=>{
+router.delete('/ninjas/:id',(req,res,next)=>{
     res.send({type:'DELETE'})
 } )
 
