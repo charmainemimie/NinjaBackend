c41ec20d4d482da6133bcdb6a49756008384292a
charmiainemimie <charmainemangorima@gmail.com>
2024-05-03 12:51:05 +0200

    put request done using findByIdAndUpdate and findOne

--- routes/api.js
+++ routes/api.js
@@ -1,34 +1,37 @@
-const express = require('express')
-const router=express.Router()
-const Ninja=require('../models/ninja')
+const express = require("express");
+const router = express.Router();
+const Ninja = require("../models/ninja");
 
 //get a list of ninjas from the db-GET
-router.get('/ninjas',(req,res,next)=>{
-    res.send({type:'GET'})
-} )
+router.get("/ninjas", (req, res, next) => {
+  res.send({ type: "GET" });
+});
 
 //add a new ninja to the db-POST
-router.post('/ninjas',(req,res,next)=>{
-    Ninja.create(req.body)  //returns a promise so use .then
-        .then((ninja)=>{
-             res.send(ninja)
-        }).catch(next) 
-} )
+router.post("/ninjas", (req, res, next) => {
+  Ninja.create(req.body) //returns a promise so use .then
+    .then((ninja) => {
+      res.send(ninja);
+    })
+    .catch(next);
+});
 
 //update a ninja in the db-PUT
-router.put('/ninjas/:id',(req,res,next)=>{
-    res.send({type:'PUT'})
-} )
+router.put("/ninjas/:id", (req, res, next) => {
+  Ninja.findByIdAndUpdate({ _id: req.params.id }, req.body).then(() => {
+    //find updated ninja
+    Ninja.findOne({ _id: req.params.id }).then((ninja) => {
+      res.send(ninja);
+    });
+  });
+});
 
 //delete a ninja from the db-DELETE
-router.delete('/ninjas/:id',(req,res,next)=>{
-//  console.log(req.params.id)
+router.delete("/ninjas/:id", (req, res, next) => {
+  //  console.log(req.params.id)
+  Ninja.findByIdAndDelete({ _id: req.params.id }).then((ninja) => {
+    res.send(ninja);
+  });
+});
 
-Ninja.findByIdAndDelete({_id:req.params.id})
-        .then((ninja)=>{
-            res.send(ninja)
-})
-   
-} )
-
-module.exports=router;
+module.exports = router;
